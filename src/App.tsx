7afa51204a480614c2584a60382bfe29d8d1170c
Nathan <natiouswright@gmail.com>
2023-07-26 13:36:01 -0500

    Init navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <>
-    <div className='bg-black w-full'>
+    <div className='bg-slate-800 w-screen h-screen'>
       <div className='flex justify-evenly'>
         <div className='text-white'>Some left content.</div>
         <div className='text-white'>Some right content.</div>
