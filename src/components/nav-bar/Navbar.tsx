90df03c887f69e2d93c3d601568d8d42bc4483d9
Nathan-422 <natiouswright@gmail.com>
2023-08-15 22:04:29 -0400

    Changed style of navbar

--- src/components/nav-bar/Navbar.tsx
+++ src/components/nav-bar/Navbar.tsx
@@ -3,7 +3,7 @@ export interface INavbarProps {
 
 export default function Navbar () { 
     return (
-    <nav className="flex flex-row justify-end space-x-4 px-6">
+    <nav className="bg-slate-600 text-white flex flex-row justify-end space-x-4 px-6 py-3">
         <a className="" href='about'>About</a>
         <a className="" href='contact'>Contact</a>
     </nav>
