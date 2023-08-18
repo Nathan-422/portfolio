c2ba59b22ad93a738f718047da46d2349c6de822
Nathan-422 <natiouswright@gmail.com>
2023-08-18 13:04:34 -0400

    Updated writing of navbar

--- src/components/nav-bar/Navbar.tsx
+++ src/components/nav-bar/Navbar.tsx
@@ -2,10 +2,20 @@ export interface INavbarProps {
 }
 
 export default function Navbar () { 
+    const routes = [
+        { id: 1, text: 'About', route: 'about', },
+        { id: 2, text: 'Contact', route: 'contact', },
+    ]
+
+    const links = routes.map( route => 
+        <a key={route.id} href={route.route}>
+            {route.text}
+        </a>
+    )
+
     return (
     <nav className="bg-slate-600 text-white flex flex-row justify-end space-x-4 px-6 py-3">
-        <a className="" href='about'>About</a>
-        <a className="" href='contact'>Contact</a>
+        {links}
     </nav>
 	)
 }
