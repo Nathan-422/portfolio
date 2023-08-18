6685bf24e4b10a705fa4a44ba8501b0c96d68b00
Nathan-422 <natiouswright@gmail.com>
2023-08-17 23:28:22 -0400

    Working on learning syntax

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,31 @@
 import Navbar from "./components/nav-bar/Navbar.tsx";
 import HomePage from "./pages/home/home.tsx";
 
+function Card() {
+
+  // variables are assigned
+  const text = "I'm the default card text"
+  const showPrimary = true
+  const cardStyles = "bg-white text-black mx-auto max-w-md p-4 rounded-md shadow-sm hover:shadow-md transition duration-500 ease-in-out"
+
+  // This if else can be done with a ternary {condition ? true : false} or a logical && syntax of {condition && trueContent}
+  if (showPrimary) {
+  return <>
+  <div className={cardStyles}>
+    {/* TS can be accessed from currly braces */}
+    {text} 
+  </div>
+  </>
+  } else {
+    return <>
+  <div className={cardStyles}>
+    This text shouldn't be displayed!
+  </div>
+  </>
+  }
+
+}
+
 function App() {
   return (
     <>
@@ -9,9 +34,9 @@ function App() {
       <div className="flex flex-col justify-center space-y-6">
         <HomePage></HomePage>
         <div className="bg-white text-black mx-auto max-w-md p-4 rounded-md shadow-sm hover:shadow-md transition duration-500 ease-in-out">I'm Nathan, an video editor transitioning to web development. I've worked for about 8 years in media production, but my days of chasing freelance work have run their course. </div>
-	<div className="bg-white text-black mx-auto max-w-md p-4 rounded-md shadow-sm hover:shadow-md transition duration-500 ease-in-out">I got into launchcode's (a non-profit I'm passionate about!) web dev bookcamp in the fall of 2022 and finished 6 months later with a basic knowledge of developing SPA webapps with Angular, and backend APIs with Springboot. After the bootcamp I built a web app called Taste Buddies with a team of six in an Agile style over two months.</div> 
-        <div className="bg-white text-black mx-auto max-w-md p-4 rounded-md shadow-sm hover:shadow-md transition duration-500 ease-in-out">Taste Buddies is a whimsical web app built out of the prompt: "how about tinder but for restaurants". The app allows you and a group to pick a time and area and swipe through restaurants until you all match.</div>
-        <div className="bg-white text-black mx-auto max-w-md p-4 rounded-md shadow-sm hover:shadow-md transition duration-500 ease-in-out"></div>
+        <div className="bg-white text-black mx-auto max-w-md p-4 rounded-md shadow-sm hover:shadow-md transition duration-500 ease-in-out">I got into launchcode's (a non-profit I'm passionate about!) web dev bookcamp in the fall of 2022 and finished 6 months later with a basic knowledge of developing SPA webapps with Angular, and backend APIs with Springboot. After the bootcamp I built a web app called Taste Buddies with a team of six in an Agile style over two months.</div> 
+        <div className="bg-white text-black mx-auto max-w-md p-4 rounded-md shadow-sm hover:shadow-md transition duration-500 ease-in-out">Taste Buddies is a whimsical web app built out of the prompt: "tinder but for restaurants". The app allows you and a group to pick a time and area and swipe through restaurants until you find a match.</div>
+        <Card />
         <div className="bg-white text-black mx-auto max-w-md p-4 rounded-md shadow-sm hover:shadow-md transition duration-500 ease-in-out"></div>
       </div>
     </div>
