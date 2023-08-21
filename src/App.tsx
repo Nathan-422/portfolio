03e00cb13fc695dee7a01537bcea2f9ba437ac42
Nathan-422 <natiouswright@gmail.com>
2023-08-21 18:37:39 -0400

    Use new card format better

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Navbar } from "./components/nav-bar/Navbar.tsx";
 import { HomePage } from "./pages/home/home.tsx";
-import { CardComp } from "./components/card/Card.tsx";
+import { Card } from "./components/card/Card.tsx";
 
 
   // variables are assigned
@@ -32,10 +32,10 @@ function App() {
       <Navbar></Navbar>
       <div className="flex flex-col justify-center space-y-6">
         <HomePage></HomePage>
-        <CardComp contents={"I'm Nathan, a video editor transitioning to web development. I've worked for about 8 years in media production, but my days of chasing freelance work have run their course."} />
-        <CardComp contents="I got into launchcode's (a non-profit I'm passionate about!) web dev bookcamp in the fall of 2022 and finished 6 months later with a basic knowledge of developing SPA webapps with Angular, and backend APIs with Springboot. After the bootcamp I built a web app called Taste Buddies with a team of six in an Agile style over two months." />
-        <CardComp contents='Taste Buddies is a whimsical web app built out of the prompt: "tinder but for restaurants". The app allows you and a group to pick a time and area and swipe through restaurants until you find a match.' />
-        <CardComp contents=""/>
+        <Card contents={"I'm Nathan, a video editor transitioning to web development. I've worked for about 8 years in media production, but my days of chasing freelance work have run their course."} />
+        <Card contents="I got into launchcode's (a non-profit I'm passionate about!) web dev bookcamp in the fall of 2022 and finished 6 months later with a basic knowledge of developing SPA webapps with Angular, and backend APIs with Springboot. After the bootcamp I built a web app called Taste Buddies with a team of six in an Agile style over two months." />
+        <Card contents='Taste Buddies is a whimsical web app built out of the prompt: "tinder but for restaurants". The app allows you and a group to pick a time and area and swipe through restaurants until you find a match.' />
+        <Card contents=""/>
     </div>
     </div>
     </>
