90e5b844b16a254a89965c0fab3b31106fee0a0e
Nathan Wright <natiouswright@gmail.com>
2023-08-23 16:51:17 -0500

    Converted card component to use children prop

--- src/App.tsx
+++ src/App.tsx
@@ -32,13 +32,12 @@ function App() {
       <Navbar></Navbar>
       <div className="flex flex-col justify-center space-y-6">
         <HomePage></HomePage>
-        <Card contents={"I'm Nathan, a video editor transitioning to web development. I've worked for about 8 years in media production, but my days of chasing freelance work have run their course."} />
-        <Card contents="I got into launchcode's (a non-profit I'm passionate about!) web dev bookcamp in the fall of 2022 and finished 6 months later with a basic knowledge of developing SPA webapps with Angular, and backend APIs with Springboot. After the bootcamp I built a web app called Taste Buddies with a team of six in an Agile style over two months." />
-        <Card contents='Taste Buddies is a whimsical web app built out of the prompt: "tinder but for restaurants". The app allows you and a group to pick a time and area and swipe through restaurants until you find a match.' />
-        <Card contents=""/>
+        <Card>I'm Nathan, a video editor transitioning to web development. I've worked for about 8 years in media production, but my days of chasing freelance work have run their course.</Card>
+        <Card>I got into launchcode's (a non-profit I'm passionate about!) web dev bookcamp in the fall of 2022 and finished 6 months later with a basic knowledge of developing SPA webapps with Angular, and backend APIs with Springboot. After the bootcamp I built a web app called Taste Buddies with a team of six in an Agile style over two months.</Card>
+        <Card>Taste Buddies is a whimsical web app built out of the prompt: "tinder but for restaurants". The app allows you and a group to pick a time and area and swipe through restaurants until you find a match.</Card>
     </div>
-    </div>
-    </>
+  </div>
+  </>
   )
 }
 
--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,11 +1,13 @@
+import React from "react";
+
 export interface ICardProps {
-    contents: string;
+    children: React.ReactNode
 }
 
 export function Card (props: ICardProps) {
   return (
     <>
-    <div className="bg-white text-black mx-auto max-w-md p-4 rounded-md shadow-sm hover:shadow-md transition duration-500 ease-in-out">     {props.contents}
+    <div className="bg-white text-black mx-auto max-w-md p-4 rounded-md shadow-sm hover:shadow-md transition duration-500 ease-in-out">{props.children}
     </div>
     </>
   );
