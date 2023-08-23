dac927dac4a065ad56cbff080ca84581ad9186db
Nathan Wright <natiouswright@gmail.com>
2023-08-23 17:27:00 -0500

    Cleaned up homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { Navbar } from "./components/nav-bar/Navbar.tsx";
-import { HomePage } from "./pages/home/home.tsx";
 import { Card } from "./components/card/Card.tsx";
 
 export default function App() {
   return (
     <>
-    <div className='bg-slate-100 text-slate-800 w-screen h-auto flex flex-col align'>
+    <div className='bg-slate-100 text-slate-800 w-screen h-full min-h-screen flex flex-col align'>
       <Navbar></Navbar>
       <div className="flex flex-col justify-center space-y-6">
-        <HomePage></HomePage>
         <Card>I'm Nathan, a video editor transitioning to web development. I've worked for about 8 years in media production, but my days of chasing freelance work have run their course.</Card>
         <Card>I got into launchcode's (a non-profit I'm passionate about!) web dev bookcamp in the fall of 2022 and finished 6 months later with a basic knowledge of developing SPA webapps with Angular, and backend APIs with Springboot. After the bootcamp I built a web app called Taste Buddies with a team of six in an Agile style over two months.</Card>
         <Card>Taste Buddies is a whimsical web app built out of the prompt: "tinder but for restaurants". The app allows you and a group to pick a time and area and swipe through restaurants until you find a match.</Card>
--- src/components/nav-bar/Navbar.tsx
+++ src/components/nav-bar/Navbar.tsx
@@ -14,7 +14,7 @@ export function Navbar () {
     )
 
     return (
-    <nav className="bg-slate-600 text-white flex flex-row justify-end space-x-4 px-6 py-3">
+    <nav className="bg-slate-600 text-white flex flex-row justify-end space-x-4 px-6 py-3 mb-6">
         {links}
     </nav>
 	)
