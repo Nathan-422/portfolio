1ee5e462fa9a69092018e2ac276391f939ed24e9
Nathan <natiouswright@gmail.com>
2023-09-02 23:32:19 -0500

    Changed content to generate with a for loop

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { Navbar } from "./components/nav-bar/Navbar.tsx";
 import { Card } from "./components/card/Card.tsx";
 
 let contents = [
-	{id: "I'm Nathan, a video editor transitioning to web development. I've worked for about 8 years in media production, but my days of chasing freelance work have run their course.", 
+	{content: "I'm Nathan, a video editor transitioning to web development. I've worked for about 8 years in media production, but my days of chasing freelance work have run their course.", 
 	 id: crypto.randomUUID()},
 	{content: "I got into launchcode's (a non-profit I'm passionate about!) web dev bookcamp in the fall of 2022 and finished 6 months later with a basic knowledge of developing SPA webapps with Angular, and backend APIs with Springboot. After the bootcamp I built a web app called Taste Buddies with a team of six in an Agile style over two months.", 
 	 id: crypto.randomUUID()},
-	{content: 'Taste Buddies is a whimsical web app built out of the prompt: "tinder but for restaurants". The app allows you and a group to pick a time and area and swipe through restaurants until you find a match.',
+	{content: "Taste Buddies is a whimsical web app built out of the prompt: \"tinder but for restaurants\". The app allows you and a group to pick a time and area and swipe through restaurants until you find a match.",
 	 id: crypto.randomUUID()},
 ]
 
@@ -18,11 +18,13 @@ export default function App() {
       <div className="flex flex-col justify-center space-y-6">
 	{contents.map((item) => {
 		return (
+			<Card 
 			{...item}
-			TODO: continue me
+			key={item.id}>
+				{item.content}
+			</Card>	
 		)
 	})}
-        <Card></Card>
     </div>
   </div>
   </>
