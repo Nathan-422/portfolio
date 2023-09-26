b6cc28dbbf2ac3fced9bedce0f81ace5051d411d
Nathan Wright <natiouswright@gmail.com>
2023-09-26 16:18:28 -0500

    Added eslint, prettier, husky, and lint-staged

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,9 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -14,5 +16,6 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'comma-dangle': ['error', 'always'],
   },
 }
