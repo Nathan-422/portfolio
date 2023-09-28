40ef41d4b395abdd3c3fcef3737f13fdb805c493
Nathan Wright <natiouswright@gmail.com>
2023-09-27 21:56:44 -0500

    Added basic routes to homepage, controlled by nav-bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
-import { Navbar } from "./components/nav-bar/Navbar.tsx";
-import { Card } from "./components/card/Card.tsx";
-import About from "./pages/about/About.tsx";
+import { Navbar } from './components/nav-bar/Navbar.tsx'
+import { Card } from './components/card/Card.tsx'
+import About from './pages/about/About.tsx'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { Home } from './pages/home/Home.tsx'
 
 const contents = [
-	{content: "I'm Nathan, a video editor transitioning to web development. I've worked for about 8 years in media production, but my days of chasing freelance work have run their course."},
-	{content: "I got into launchcode's (a non-profit I'm passionate about!) web dev bookcamp in the fall of 2022 and finished 6 months later with a basic knowledge of developing SPA webapps with Angular, and backend APIs with Springboot. After the bootcamp I built a web app called Taste Buddies with a team of six in an Agile style over two months."},
-	{content: "Taste Buddies is a whimsical web app built out of the prompt: \"tinder but for restaurants\". The app allows you and a group to pick a time and area and swipe through restaurants until you find a match."},
+  {
+    content:
+      "I'm Nathan, a video editor transitioning to web development. I've worked for about 8 years in media production, but my days of chasing freelance work have run their course.",
+  },
+  {
+    content:
+      "I got into launchcode's (a non-profit I'm passionate about!) web dev bookcamp in the fall of 2022 and finished 6 months later with a basic knowledge of developing SPA webapps with Angular, and backend APIs with Springboot. After the bootcamp I built a web app called Taste Buddies with a team of six in an Agile style over two months.",
+  },
+  {
+    content:
+      'Taste Buddies is a whimsical web app built out of the prompt: "tinder but for restaurants". The app allows you and a group to pick a time and area and swipe through restaurants until you find a match.',
+  },
 ]
 
 export default function App() {
   return (
-    <>
-    <Navbar></Navbar>
-    <div className='pt-6 px-1'>
-      <About/>
-      {/* <div className="flex flex-col justify-center space-y-6">
+    <Router>
+      <Navbar></Navbar>
+      <div className="container m-auto pt-6 px-1">
+        <Routes>
+          <Route path="/" element={<LittleHome />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="*" element={<NoMatch />}></Route>
+        </Routes>
+        {/* <div className="flex flex-col justify-center space-y-6">
         {contents.map((item) => {
           return (
             <Card 
@@ -25,7 +40,25 @@ export default function App() {
           )
         })}
       </div> */}
-    </div>
-  	</>
+      </div>
+    </Router>
+  )
+}
+
+function LittleHome() {
+  return (
+    <>
+      <h1 className="text-6xl">Home</h1>
+      <p>Welcome to Nathan's router</p>
+    </>
+  )
+}
+
+function NoMatch() {
+  return (
+    <>
+      <h1 className="text-6xl text-red-700">404</h1>
+      <p>Sorry, the hampsters couldn't find that page</p>
+    </>
   )
 }
--- src/components/nav-bar/Navbar.tsx
+++ src/components/nav-bar/Navbar.tsx
@@ -1,24 +1,27 @@
+import { Link } from 'react-router-dom'
+
 export interface INavbarProps {}
 
 export function Navbar() {
   const routes = [
-    { id: 1, text: 'About', route: 'about' },
-    { id: 2, text: 'Contact', route: 'contact' },
+    { text: 'Home', route: '/' },
+    { text: 'About', route: '/about' },
+    { text: 'Contact', route: '/contact' },
   ]
 
   const links = routes.map((route) => (
-    <a key={route.id} href={route.route}>
+    <Link key={crypto.randomUUID()} to={route.route}>
       {route.text}
-    </a>
+    </Link>
   ))
 
-    return (
+  return (
     <div className="bg-slate-600">
-        <div className="max-w-5xl mx-auto">
-            <nav className=" text-white flex flex-row justify-end content-center space-x-4 px-6 py-3">
-                {links}
-            </nav>
-        </div>
+      <div className="max-w-5xl mx-auto">
+        <nav className=" text-white flex flex-row justify-end content-center space-x-4 px-6 py-3">
+          {links}
+        </nav>
+      </div>
     </div>
-	)
+  )
 }
