cb8f4428c77d328e0f6fc64581f1b98b003a990b
Nathan Wright <natiouswright@gmail.com>
2023-10-01 19:34:02 -0500

    Added tailwind prettier extention and cleared readme

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,33 +2,27 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    es2020: true, },
+    es2020: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
-    'plugin:react/jsx-runtime'
+    'plugin:react/jsx-runtime',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: [
-    'react', 
-    'react-hooks', 
-    'react-refresh'
-  ],
+  plugins: ['react', 'react-hooks', 'react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    'react/react-in-jsx-scope': "off",
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/react-in-jsx-scope': 'off',
   },
   settings: {
     react: {
-      fragment: "syntax",
-      version: "detect"
-    }
-  } 
+      fragment: 'syntax',
+      version: 'detect',
+    },
+  },
 }
