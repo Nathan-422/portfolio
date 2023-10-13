fd51ef78d901cb47369ecd12a530e9712d2e26c9
Nathan Wright <natiouswright@gmail.com>
2023-10-13 13:40:17 -0500

    Prettier and fixing apostrophes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <Router>
       <Navbar></Navbar>
-      <div className="m-auto pt-6 font-sans">
+      <div className="font-san m-auto pt-6">
         <Routes>
           <Route path="/" element={<LittleHome />}></Route>
           <Route path="/about" element={<About />}></Route>
@@ -21,7 +21,7 @@ function LittleHome() {
   return (
     <>
       <h1 className="text-6xl">Home</h1>
-      <p>Welcome to Nathan$apos;s router</p>
+      <p>Welcome to Nathan&apos;s router</p>
     </>
   )
 }
@@ -30,7 +30,7 @@ function NoMatch() {
   return (
     <>
       <h1 className="text-6xl text-red-700">404</h1>
-      <p>Sorry, the hampsters couldn$apos;t find that page</p>
+      <p>Sorry, the hampsters couldn&apos;t find that page</p>
     </>
   )
 }
